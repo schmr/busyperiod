212c9cd7b606d5ec91d92d2dfa81b6194f7c0d1e
Robert Schmidt <8289229+schmr@users.noreply.github.com>
2021-05-05 22:11:51 +0200

    Fix progress indicator

--- cmd/busyp/main.go
+++ cmd/busyp/main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	fmt.Println(os.Args)
-	for k := 0; k < 100; k++ {
+	for k := 0; k < 100000; k++ {
+		fmt.Print(".")
 		// Generator
 		d := taskset.CreateRandomDualCritMin()
 		// EDF-NUVD check and scaling
@@ -17,20 +18,23 @@ func main() {
 		if !ok {
 			continue
 		}
+		fmt.Print("s")
 		// Busy period check
 		checkpoints, ok := taskset.BuildCheckpoints(&d)
 		if !ok {
 			continue
 		}
+		fmt.Print("c")
 		for _, t := range checkpoints {
 			w := taskset.WorkBound(&d, float64(t))
 			if w > float64(t) {
 				fmt.Printf("\ntaskset not EDF schedulable according to busy period check:\n%v\n", d)
 				fmt.Printf("checkpoints: %v\n", checkpoints)
 				fmt.Printf("checked t: %v\n", t)
-				break
+				os.Exit(0)
 			}
 		}
-		fmt.Print(".")
+		fmt.Print("E")
 	}
+	fmt.Print("\n")
 }
