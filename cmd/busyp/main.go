285aceb266a033555ecf6dbc3d4e293cf4e6b6f1
Robert Schmidt <8289229+schmr@users.noreply.github.com>
2021-05-06 14:15:32 +0200

    Try concurrency with waiting for all to finish

--- cmd/busyp/main.go
+++ cmd/busyp/main.go
@@ -4,38 +4,81 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/schmr/busyperiod/taskset"
 )
 
+// set by linker
+var revisiondate string
+var revision string
+
 func main() {
-	fmt.Println(os.Args)
-	for k := 0; k < 100000; k++ {
-		fmt.Print(".")
-		// Generator
-		d := taskset.CreateRandomDualCritMin()
-		// EDF-NUVD check and scaling
-		ok := d.ScaleTasksetEDFNUVD()
-		if !ok {
-			continue
+	// Maybe there is a package for this I am not aware of?
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "-v":
+			fmt.Println("busyp", revision, revisiondate)
+			os.Exit(0)
+		case "-h":
+			fmt.Println("busyp [-v] [-h]")
+			fmt.Println("\t-v Show version information")
+			fmt.Println("\t-h Show this help")
+			os.Exit(0)
+		default:
+			fmt.Println("Ignoring unknown argument.")
 		}
-		fmt.Print("s")
-		// Busy period check
-		checkpoints, ok := taskset.BuildCheckpoints(&d)
-		if !ok {
-			continue
-		}
-		fmt.Print("c")
-		for _, t := range checkpoints {
-			w := taskset.WorkBound(&d, float64(t))
-			if w > float64(t) {
-				fmt.Printf("\ntaskset not EDF schedulable according to busy period check:\n%v\n", d)
-				fmt.Printf("checkpoints: %v\n", checkpoints)
-				fmt.Printf("checked t: %v\n", t)
-				os.Exit(0)
-			}
-		}
-		fmt.Print("E")
 	}
+	tries := 100000
+
+	type violator struct {
+		TaskSet taskset.DualCritMin
+		Checkpoints []int
+		ViolatedAt int
+	}
+	violators := make(chan violator)
+	var wg sync.WaitGroup
+	for i := 0; i < tries; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			fmt.Printf(" %d ", id)
+			// Generator
+			d := taskset.CreateRandomDualCritMin()
+			// EDF-NUVD check and scaling
+			ok := d.ScaleTasksetEDFNUVD()
+			if !ok {
+				return
+			}
+			// Busy period check
+			checkpoints, ok := taskset.BuildCheckpoints(&d)
+			if !ok {
+				return
+			}
+			for _, t := range checkpoints {
+				w := taskset.WorkBound(&d, float64(t))
+				if w > float64(t) {
+					var vr violator
+					vr.TaskSet = d
+					vr.Checkpoints = checkpoints
+					vr.ViolatedAt = t
+					violators <- vr
+				}
+			}
+			fmt.Printf(" %d(E) ", id)
+		}(i)
+	}
+
+	// wait for all to finish; rewrite to end program on first found counterexample
+	go func() {
+		wg.Wait()
+		close(violators)
+	}()
+
 	fmt.Print("\n")
+	for v := range violators {
+		fmt.Printf("\ntaskset not EDF schedulable according to busy period check:\n%v\n", v.TaskSet)
+		fmt.Printf("checkpoints: %v\n", v.Checkpoints)
+		fmt.Printf("checked t: %v\n", v.ViolatedAt)
+	}
 }
