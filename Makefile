3781a65c34a5298bb9de5b2845e074a8bda1a14d
Robert Schmidt <8289229+schmr@users.noreply.github.com>
2021-05-07 14:29:48 +0200

    Allow endless search and concurrent result printing

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 gitdate := $(shell git log -1 --format=\"%ad\" --date=short)
 gitrevision := $(shell git log -1 --format=\"%h\")
 
-.PHONY: run test lint
+.PHONY: run test lint race
 
 busyp: cmd/busyp/main.go taskset/taskset.go | test
 	go build -o $@ -ldflags "-X main.revisiondate=${gitdate} -X main.revision=${gitrevision}" $<
@@ -15,3 +15,7 @@ test: taskset/taskset.go taskset/printing_test.go taskset/calculation_test.go | 
 lint: taskset/taskset.go taskset/printing_test.go taskset/calculation_test.go
 	goimports -w $^
 	golint -set_exit_status
+
+race: cmd/busyp/main.go taskset/taskset.go | test
+	go run -race cmd/busyp/main.go
+
--- cmd/busyp/main.go
+++ cmd/busyp/main.go
@@ -2,23 +2,29 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"flag"
-	"runtime"
+	"fmt"
 	"math/rand"
+	"os"
+	"runtime"
 	"time"
 
-	"github.com/schmr/busyperiod/taskset"
 	"github.com/gammazero/workerpool"
+	"github.com/schmr/busyperiod/taskset"
 )
 
 // set by linker
 var revisiondate string = "unknown"
 var revision string = "unknown"
 
+type violator struct {
+	TaskSet     taskset.DualCritMin
+	Checkpoints []int
+	ViolatedAt  int
+}
+
 func main() {
-	var tries = flag.Int("n", 100000, "number of attempts to find a counterexample")
+	var tries = flag.Int("n", 100000, "number of attempts to find a counterexample or zero for endless search")
 	var showversion = flag.Bool("v", false, "show version information and exit")
 	flag.Parse()
 
@@ -30,51 +36,64 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	numWorkers := runtime.NumCPU()
 	wp := workerpool.New(numWorkers)
-
-	type violator struct {
-		TaskSet taskset.DualCritMin
-		Checkpoints []int
-		ViolatedAt int
-	}
 	violators := make(chan violator, numWorkers)
-	for i := 0; i < *tries; i++ {
-		wp.Submit(func() {
-			// Generator
-			d := taskset.CreateRandomDualCritMin()
-			// EDF-NUVD check and scaling
-			ok := d.ScaleTasksetEDFNUVD()
-			if !ok {
-				return
-			}
-			// Busy period check
-			checkpoints, ok := taskset.BuildCheckpoints(&d)
-			if !ok {
-				return
-			}
-			for _, t := range checkpoints {
-				w := taskset.WorkBound(&d, float64(t))
-				if w > float64(t) { // found counterexample
-					var vr violator
-					vr.TaskSet = d
-					vr.Checkpoints = checkpoints
-					vr.ViolatedAt = t
-					violators <- vr
-				}
-			}
-		})
-	}
 
-	// check in parallel if we already have a result and print it
-	// don't want to wait for all goroutines to finish prior to printing
-	// don't track in WaitGroup wg, if no counterexample is found this
-	// routine never returns and would stall the WaitGroup
+	// Check in parallel if we already have a result and print it.
+	// Don't want to wait for all goroutines to finish prior to printing.
+	// Don't track in WaitGroup or run in workerpool, if no counterexample
+	// is found this routine never returns and would stall the program.
 	go func() {
-		for v := range violators {
+		for {
+			v, ok := <-violators
+			if !ok {
+				break
+			}
 			fmt.Printf("\ntaskset not EDF schedulable according to busy period check:\n%v\n", v.TaskSet)
 			fmt.Printf("checkpoints: %v\n", v.Checkpoints)
 			fmt.Printf("checked t: %v\n", v.ViolatedAt)
 		}
 	}()
 
+	// put all jobs in queue; how many jobs can I put here without problems?
+	if *tries > 0 {
+		for i := 0; i < *tries; i++ {
+			wp.Submit(func() {
+				searchCounterExample(violators)
+			})
+		}
+	} else { // endless generation
+		for {
+			wp.Submit(func() {
+				searchCounterExample(violators)
+			})
+		}
+	}
+
 	wp.StopWait()
+}
+
+// a complete chain to discover a counterexample
+func searchCounterExample(out chan<- violator) {
+	// Generator
+	d := taskset.CreateRandomDualCritMin()
+	// EDF-NUVD check and scaling
+	ok := d.ScaleTasksetEDFNUVD()
+	if !ok {
+		return
+	}
+	// Busy period check
+	checkpoints, ok := taskset.BuildCheckpoints(&d)
+	if !ok {
+		return
+	}
+	for _, t := range checkpoints {
+		w := taskset.WorkBound(&d, float64(t))
+		if w > float64(t) { // found counterexample
+			var vr violator
+			vr.TaskSet = d
+			vr.Checkpoints = checkpoints
+			vr.ViolatedAt = t
+			out <- vr
+		}
+	}
 }
